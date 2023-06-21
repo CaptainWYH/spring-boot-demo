a314fbc81298bf660249e8dc7564f864b94fc522
weiyuhui <1264787656@qq.com>
2023-06-21 11:34:19 +0800

    ✨ java-modbus 模块新建

---
+++ demo-modbus/src/main/java/com/wyhcode/Main.java
@@ -0,0 +1,13 @@
+package com.wyhcode;
+
+/**
+ * @author weiyuhui
+ * @date 2023/6/21 11:09
+ * @description
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
