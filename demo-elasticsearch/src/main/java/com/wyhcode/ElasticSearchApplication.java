a966204666ea04e1b6c69693e06469fbb23d5718
weiyuhui <1264787656@qq.com>
2023-07-25 18:07:55 +0800

    ✨ Elasticsearch 集成

---
+++ demo-elasticsearch/src/main/java/com/wyhcode/ElasticSearchApplication.java
@@ -0,0 +1,23 @@
+package com.wyhcode;
+
+import com.google.common.base.Stopwatch;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author weiyuhui
+ * @date 2023/7/25 14:29
+ * @description
+ */
+
+@SpringBootApplication
+@Slf4j
+public class ElasticSearchApplication {
+    public static void main(String[] args) {
+        Stopwatch started = Stopwatch.createStarted();
+        SpringApplication.run(ElasticSearchApplication.class, args);
+        started.stop();
+        log.info("项目启动耗时{}s",started.elapsed().getSeconds());
+    }
+}
