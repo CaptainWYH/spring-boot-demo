04546008fd6e5457b52a7dbeed7df5297d4dc888
weiyuhui <weiyuhui@induschain.cn>
2023-08-22 15:07:21 +0800

    ✨ mybatis-plus starter

---
+++ demo-starter/demo-starter-mybatis-plus/src/main/java/com/wyhcode/handler/MyMetaObjectHandler.java
@@ -0,0 +1,24 @@
+package com.wyhcode.handler;
+
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import org.apache.ibatis.reflection.MetaObject;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class MyMetaObjectHandler implements MetaObjectHandler {
+
+    @Override
+    public void insertFill(MetaObject metaObject) {
+        this.strictInsertFill(metaObject, "createBy", String.class, "weiyuhui");
+        this.strictInsertFill(metaObject, "createTime", Date.class, new Date());
+        this.strictInsertFill(metaObject, "deleteFlag", Integer.class, 0);
+    }
+
+    @Override
+    public void updateFill(MetaObject metaObject) {
+        this.strictUpdateFill(metaObject, "updateBy", String.class, "weiyuhui");
+        this.strictUpdateFill(metaObject, "updateTime", Date.class, new Date());
+    }
+}
